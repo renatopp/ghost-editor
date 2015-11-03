9a7cf537c0b888e610c29a193df550d459bc9338
Renato Pereira <renato.ppontes@gmail.com>
2015-11-03 13:59:55 -0200

    User can create connections again (multiconnection)

--- src/editor/connection/DisplayComponent.js
+++ src/editor/connection/DisplayComponent.js
@@ -7,7 +7,8 @@
     this._settings = null;
 
     this.connection = connection;
-    this.shape = null;
+    this.shape = new createjs.Shape();
+    this.addChild(this.shape);
   };
   var p = createjs.extend(DisplayComponent, createjs.Container);
 
@@ -30,12 +31,15 @@
    * @protected
    */
   p.redraw = function(x1, y1, x2, y2) {
-    if (! ((this._inBlock||x1||y1) && (this._outBlock||x2||y2)) ) {
+    var inNode = this.connection.inNode;
+    var outNode = this.connection.outNode;
+
+    if (! ((inNode||x1||y1) && (outNode||x2||y2)) ) {
       return;
     }
 
     var s          = this._settings;
-    var graphics   = this.graphics;
+    var graphics   = this.shape.graphics;
     var width      = s.get('connection_width');
     var color      = s.get('connection_color');
     var diff       = s.get('anchor_radius') + s.get('anchor_border_width');
@@ -43,27 +47,27 @@
     var layout     = s.get('layout');
 
     var dx=0; var dy=0; var angle=0; var ax=0; var ay=0;
-    // var inAnchor = this._outBlock._getInAnchorPosition();
-    // var outAnchor = this._inBlock._getOutAnchorPosition();
+    // var inAnchor = outNode._getInAnchorPosition();
+    // var outAnchor = inNode._getOutAnchorPosition();
 
     if (!(x1 === 0||x1)) {
-      var outAnchor = this._inBlock.display.getOutAnchorPosition();
+      var outAnchor = inNode.display.getOutAnchorPosition();
       if (layout === 'horizontal') {
         x1 = outAnchor.x;
-        y1 = this._inBlock.display.y;
+        y1 = inNode.display.y;
       } else {
-        x1 = this._inBlock.display.x;
+        x1 = inNode.display.x;
         y1 = outAnchor.y;
       }
     }
 
     if (!(x2 === 0||x2)) {
-      var inAnchor = this._outBlock.display.getInAnchorPosition();
+      var inAnchor = outNode.display.getInAnchorPosition();
       if (layout === 'horizontal') {
         x2 = inAnchor.x - diff;
-        y2 = this._outBlock.display.y;
+        y2 = outNode.display.y;
       } else {
-        x2 = this._outBlock.display.x;
+        x2 = outNode.display.x;
         y2 = inAnchor.y - diff;
       }
     }
--- src/editor/editor/systems/ConnectionSystem.js
+++ src/editor/editor/systems/ConnectionSystem.js
@@ -1,122 +1,178 @@
 b3e.editor.ConnectionSystem = function(editor) {
   "use strict";
 
-  var connection = null;
-  var lastOutBlock = null;
+  var connections = [];
+  var lastOutNode = null;
 
   this.update = function(delta) {};
 
   this.onMouseDown = function(e) {
+    // Accepts only left click
     if (e.nativeEvent.which !== 1) return;
 
+    // Get current project
     var project = editor.project.get();
     if (!project) return;
 
+    // Get selected tree
     var tree = project.trees.getSelected();
     if (!tree) return;
 
-    // if clicked on node
+    // Get node under the click
     var point = tree.view.getLocalPoint();
     var x = point.x;
     var y = point.y;
     var node = tree.nodes.getUnderPoint(x, y);
 
-    if (connection || !node) return;
+    // Return if clicked on empty
+    if (connections.length || !node) return;
 
+    // User left click on OUT anchor
     if (node.display.hitOutAnchor(x, y)) {
-      // if user clicked at the outAnchor
-      connection = tree.connections.add(node, null);
+      var c = tree.connections.add(node, null);
+      connections.push(c);
 
+    // User left click on IN anchor
     } else if (node.display.hitInAnchor(x, y)) {
-      // if user clicked at the inAnchor
-      var c = node._inConnection;
-      if (!c)
-          return;
+      if (!node.inConnections.length) return;
 
-      node._inConnection = null;
-      c._outBlock = null;
-      lastOutBlock = node;
+      connections = node.inConnections;
+      node.inConnections = [];      
+      lastOutNode = node;
+      for (var i=0; i<connections.length; i++) {
+        connections[i].outNode = null;
+      }
 
-      connection = c;
     }
   };
 
   this.onMouseMove = function(e) {
-    // if no connection, return
-    if (!connection) return;
+    // Return if not draggin any connection
+    if (!connections.length) return;
 
+    // Get current project
     var project = editor.project.get();
     if (!project) return;
 
+    // Get selected tree
     var tree = project.trees.getSelected();
     if (!tree) return;
 
+    // Get current mouse position
     var point = tree.view.getLocalPoint();
     var x = point.x;
     var y = point.y;
 
-    // redraw
-    connection._redraw(null, null, x, y);
+    // Redraw all dragging connections
+    for (var i=0; i<connections.length; i++) {
+      connections[i].display.redraw(null, null, x, y);
+    }
   };
 
   this.onMouseUp = function(e) {
+    // Accepts only left click
     if (e.nativeEvent.which !== 1) return;
 
-    // if no connection, return
-    if (!connection) return;
+    // Return if there is no connection
+    if (!connections.length) return;
 
+    // Get current project
     var project = editor.project.get();
     if (!project) return;
 
+    // Get selected tree
     var tree = project.trees.getSelected();
     if (!tree) return;
 
-
+    // Get node under cursor, if any
     var point = tree.view.getLocalPoint();
     var x = point.x;
     var y = point.y;
-    var block = tree.blocks.getUnderPoint(x, y);
+    var node = tree.nodes.getUnderPoint(x, y);
 
-    // if not connection or connection but no block
+    //
     project.history._beginBatch();
-    if (!block || block === connection._inBlock || block.category === 'root') {
-      if (lastOutBlock) {
-        // Add again to connection in order to create history 
-        lastOutBlock._inConnection = connection;
-        connection._outBlock = lastOutBlock;
-      }
-      tree.connections.remove(connection);
-    } else {
-      var c;
 
-      // if double parent on node
-      if (block._inConnection) {
+    for (var i=0; i<connections.length; i++) {
+      var connection = connections[i];
+      var inNode = connection.inNode;
 
-        c = block._inConnection;
-        tree.connections.remove(c);
+      // Remove connection if there is no node
+      if (!node) {
+        tree.connections.remove(connection);
+        continue;
       }
 
-      // if double children on root
-      if ((connection._inBlock.category === 'root' ||
-           connection._inBlock.category === 'decorator') &&
-           connection._inBlock._outConnections.length > 1) {
-
-        c = connection._inBlock._outConnections[0];
-        tree.connections.remove(c);
+      // Remove connection if node is connecting itself
+      if (node === inNode) {
+        tree.connections.remove(connection);
+        continue;
       }
 
-      connection._outBlock = block;
-      block._inConnection = connection;
+      // Remove connection if nodes are already connected
+      var doubleConnection = false;
+      for (var j=0; j<inNode.outConnections.length; j++) {
+        var outNode = inNode.outConnections[j].outNode;
+        if (outNode === node) {
+          tree.connections.remove(connection);
+          doubleConnection = true;
+          break;
+        }
+      }
 
-      var _old = [tree.connections, tree.connections._remove, [block]];
-      var _new = [tree.connections, tree.connections.add, [connection._inBlock, block]];
+      if (doubleConnection) {
+        continue;
+      }
+
+      // Do the connection
+      connection.outNode = node;
+      node.inConnections.push(connection);
+      var _old = [tree.connections, tree.connections._remove, [node]];
+      var _new = [tree.connections, tree.connections.add, [inNode, node]];
       project.history._add(new b3e.Command(_old, _new));
-
-      connection._redraw();
+      
+      connection.display.redraw();
     }
+
+
+    // if (!block || block === connection._inBlock || block.category === 'root') {
+    //   if (lastOutNode) {
+    //     // Add again to connection in order to create history 
+    //     lastOutNode._inConnection = connection;
+    //     connection._outBlock = lastOutNode;
+    //   }
+    //   tree.connections.remove(connection);
+    // } else {
+    //   var c;
+
+    //   // if double parent on node
+    //   if (block._inConnection) {
+
+    //     c = block._inConnection;
+    //     tree.connections.remove(c);
+    //   }
+
+    //   // if double children on root
+    //   if ((connection._inBlock.category === 'root' ||
+    //        connection._inBlock.category === 'decorator') &&
+    //        connection._inBlock._outConnections.length > 1) {
+
+    //     c = connection._inBlock._outConnections[0];
+    //     tree.connections.remove(c);
+    //   }
+
+    //   connection._outBlock = block;
+    //   block._inConnection = connection;
+
+    //   var _old = [tree.connections, tree.connections._remove, [block]];
+    //   var _new = [tree.connections, tree.connections.add, [connection._inBlock, block]];
+    //   project.history._add(new b3e.Command(_old, _new));
+
+    //   connection._redraw();
+    // }
     project.history._endBatch();
 
-    connection = null;
+    connections = [];
   };
 
   editor._game.stage.on('stagemousedown', this.onMouseDown, this);
