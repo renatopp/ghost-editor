a07e7b041d8697f4cfa2a05b9206002f31a9d512
Renato Pereira <renato.ppontes@gmail.com>
2015-11-09 20:44:46 -0200

    Exporting routines

--- src/editor/editor/managers/ExportManager.js
+++ src/editor/editor/managers/ExportManager.js
@@ -28,46 +28,26 @@ b3e.editor.ExportManager = function(editor) {
     if (!project) return;
 
     var tree = project.trees.getSelected();
-
     var data = {
       version      : b3e.VERSION,
-      scope        : 'project',
       selectedTree : (tree?tree._id:null),
       trees        : []
-      // custom_nodes : this.nodesToData()
     };
 
+    var self = this;
     project.trees.each(function(tree) {
-      var d = this.treeToData(tree, true);
-      d.id = tree._id;
-      data.trees.push(d);
+      data.trees.push(self.treeToData(tree));
     }, this);
 
     return data;
   };
   
-  this.treeToData = function(tree, ignoreNodes) {
-    var project = editor.project.get();
-    if (!project) return;
-
-    if (!tree) {
-      tree = project.trees.getSelected();
-    } else {
-      tree = project.trees.get(tree);
-      if (!tree) return;
-    }
-
+  this.treeToData = function(tree) {
     var root = tree.nodes.getRoot();
-    var first = getBlockChildrenIds(root);
     var data = {
-      version      : b3e.VERSION,
-      scope        : 'tree',
-      id           : tree._id,
-      title        : root.title,
-      description  : root.description,
-      root         : first[0] || null,
-      properties   : root.properties,
-      nodes        : {},
+      id          : tree._id,
+      root        : root.id,
+      nodes       : [],
       display     : {
         camera_x : tree.x,
         camera_y : tree.y,
@@ -77,56 +57,31 @@ b3e.editor.ExportManager = function(editor) {
       },
     };
 
-    if (!ignoreNodes) {
-      data.custom_nodes = this.nodesToData();
-    }
-
+    var self = this;
     tree.nodes.each(function(node) {
-      if (node.category !== b3e.ROOT) {
-        var d ={
-          id          : node.id,
-          name        : node.name,
-          title       : node.title,
-          description : node.description,
-          properties  : node.properties,
-          display     : {x:node.display.x, y:node.display.y}
-        };
-
-        var children = getBlockChildrenIds(node);
-        if (node.category === b3e.COMPOSITE) {
-          d.children = children;
-        } else if (node.category === b3e.MODULATOR) {
-          d.child = children[0];
-        }
-
-        data.nodes[node.id] = d;
-      }
+      data.nodes.push(self.nodeToData(node));
     });
 
     return data;
   };
 
-  // this.nodesToData = function() {
-  //   var project = editor.project.get();
-  //   if (!project) return;
+  this.nodeToData = function(node) {
+    var data = {
+      id          : node.id,
+      name        : node.name,
+      category    : node.category,
+      title       : node.title,
+      description : node.description,
+      properties  : node.properties,
+      children    : getBlockChildrenIds(node),
+      display: {
+        x: node.display.x,
+        y: node.display.y
+      }
+    };
 
-  //   var data = [];
-  //   project.nodes.each(function(node) {
-  //     if (!node.isDefault) {
-  //       data.push({
-  //         version     : b3e.VERSION,
-  //         scope       : 'node',
-  //         name        : node.name,
-  //         category    : node.category,
-  //         title       : node.title,
-  //         description : node.description,
-  //         properties  : node.properties,
-  //       });
-  //     }
-  //   });
-
-  //   return data;
-  // };
+    return data;
+  };
 
   this.nodesToJavascript = function() {};
 
