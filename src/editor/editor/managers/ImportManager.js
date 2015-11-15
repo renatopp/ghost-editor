6a09829d26134aaf57471f6194194def3d3ad031
Renato Pereira <renato.ppontes@gmail.com>
2015-11-15 13:17:56 -0200

    Importing project using new json format

--- src/editor/editor/managers/ImportManager.js
+++ src/editor/editor/managers/ImportManager.js
@@ -5,7 +5,12 @@ b3e.editor.ImportManager = function(editor) {
     var project = editor.project.get();
     if (!project) return;
 
-    if (data.trees) this.treesAsData(data.trees);
+    if (data.trees) {
+      for (var i=0; i<data.trees.length; i++) {
+        this.treeAsData(data.trees[i]);
+      }
+    }
+
     if (data.selectedTree) {
       project.trees.select(data.selectedTree);
     }
@@ -27,61 +32,39 @@ b3e.editor.ImportManager = function(editor) {
     tree.scaleX      = display.camera_z || 1;
     tree.scaleY      = display.camera_z || 1;
 
-    root.title       = data.title;
-    root.description = data.description;
-    root.properties  = data.properties;
-    root.x           = display.x || 0;
-    root.y           = display.y || 0;
-
-    var id, spec;
-
-    // Add nodes
-    for (id in data.nodes) {
-      spec = data.nodes[id];
-      var node = null;
+    // Nodes
+    var node;
+    var spec;
+    var i;
+    for (i=0; i<data.nodes.length; i++) {
+      spec = data.nodes[i];
       display = spec.display || {};
 
-      node = tree.nodes.add(spec.name, spec.display.x, spec.display.y);
+      if (spec.id === data.root) {
+        node = root;
+        root.x = display.x || 0;
+        root.y = display.y || 0;
+      } else {
+        node = tree.nodes.add(spec.name, spec.display.x, spec.display.y);
+      } 
       node.id = spec.id;
       node.title = spec.title;
       node.description = spec.description;
       node.properties = tine.merge({}, node.properties, spec.properties);
       node.display.redraw();
-      
-      if (spec.id === data.root) {
-        first = node;
-      }
     }
 
-    // Add connections
-    for (id in data.nodes) {
-      spec = data.nodes[id];
-      var inNode = tree.nodes.get(id);
+    // Connections
+    for (i=0; i<data.nodes.length; i++) {
+      spec = data.nodes[i];
 
-      var children = null;
-      if (inNode.category === b3e.COMPOSITE && spec.children) {
-        children = spec.children;
-      }
-      else if (spec.child && (inNode.category === b3e.MODULATOR ||
-                              inNode.category === b3e.ROOT)) {
-        children = [spec.child];
-      }
-      
-      if (children) {
-        for (var i=0; i<children.length; i++) {
-          var outNode = tree.nodes.get(children[i]);
-          tree.connections.add(inNode, outNode);
-        }
-      }
-    }
+      var inNode = tree.nodes.get(spec.id);
 
-    // Finish
-    if (first) {
-      tree.connections.add(root, first);
-    }
-
-    if (!data.display) {
-      tree.organize.organize(true);
+      if (!spec.children) continue;
+      for (var j=0; j<spec.children.length; j++) {
+        var outNode = tree.nodes.get(spec.children[j]);
+        tree.connections.add(inNode, outNode);
+      }
     }
 
     tree.selection.deselectAll();
@@ -89,12 +72,6 @@ b3e.editor.ImportManager = function(editor) {
     project.history.clear();
 
     editor.trigger('treeimported');
-  };
-
-  this.treesAsData = function(data) {
-    for (var i=0; i<data.length; i++) {
-      this.treeAsData(data[i]);
-    }
   };
 
   this._applySettings = function(settings) {};
