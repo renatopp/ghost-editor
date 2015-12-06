f1e064dba8ffe0df915013c78f063561cb0bd97a
Renato Pereira <renato.ppontes@gmail.com>
2015-12-06 19:05:54 -0200

    Correct description

--- src/editor/nodes/Switch.js
+++ src/editor/nodes/Switch.js
@@ -2,9 +2,10 @@
   'use strict';
 
   b3e.nodes.Switch = b3e.node.create('Switch', b3e.COMPOSITE, {
-    description: 'Executes and immediately returns children node status, '+
-                 'one-by-one. Step iterator always moves forward by one and '+
-                 'loops it\'s index.',
+    description: 'Executes ONE child based on the provided INT or ENUM and '+
+                 'return it\'s status. If "case" change while a child is '+
+                 'running, that child will be interrupted before the new '+
+                 'child is executed.',
     properties: [
       ['selection_mode', b3e.properties.Choice, {
         value: 'enum',
