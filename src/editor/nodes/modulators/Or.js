ee497041151e6e1c6707c8f1d407ce80b5b59167
Renato Pereira <renato.ppontes@gmail.com>
2015-12-13 22:59:31 -0200

    Or node implemented

---
+++ src/editor/nodes/modulators/Or.js
@@ -0,0 +1,38 @@
+(function() {
+  'use strict';
+
+  b3e.nodes.Or = b3e.node.create('Or', b3e.MODULATOR, {
+    description: 'A logical OR.',
+    properties: [
+      ['negate', b3e.properties.Boolean, {
+        title: 'Negate',
+        description: 'Sets node to be a NOR',
+        value: false
+      }],
+      ['exclusive', b3e.properties.Boolean, {
+        title: 'Exclusive',
+        description: 'Sets node to be a XOR',
+        value: false
+      }]
+    ],
+    getTitle: function() {
+      if (this.title) {
+        return this.callSuper();
+      }
+
+      var title = 'OR';
+      if (this.properties.negate.value) {
+        title = 'N'+title;
+      }
+      if (this.properties.exclusive.value) {
+        title = 'X'+title;
+      }
+
+      if (b3e.ENV === b3e.DEVELOPMENT) {
+        title += ' <'+this.id+'>';
+      }
+
+      return title;
+    },
+  });
+})();
