58891bb347d2c1dba614978cabac188fc9545d69
Renato Pereira <renato.ppontes@gmail.com>
2015-12-13 23:09:58 -0200

    Space on description

--- src/editor/nodes/modulators/UntilFailure.js
+++ src/editor/nodes/modulators/UntilFailure.js
@@ -3,7 +3,7 @@
 
   b3e.nodes.UntilFailure = b3e.node.create('UntilFailure', b3e.MODULATOR, {
     title: 'Until Failure',
-    description: 'The until failure task, will keep executing its child task'+
+    description: 'The until failure task, will keep executing its child task '+
                  'until the child task returns failure.',
   });
 })();
--- src/editor/nodes/modulators/UntilSuccess.js
+++ src/editor/nodes/modulators/UntilSuccess.js
@@ -3,7 +3,7 @@
 
   b3e.nodes.UntilSuccess = b3e.node.create('UntilSuccess', b3e.MODULATOR, {
     title: 'Until Success',
-    description: 'The until success task, will keep executing its child task'+
+    description: 'The until success task, will keep executing its child task '+
                  'until the child task returns success.',
   });
 })();
