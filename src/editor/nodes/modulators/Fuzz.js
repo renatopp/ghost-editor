111da68e0a7110ac64eaa84da20dffd2e6e83c19
Renato Pereira <renato.ppontes@gmail.com>
2016-01-24 20:47:29 -0200

    Proper parameter values

--- src/editor/nodes/modulators/Fuzz.js
+++ src/editor/nodes/modulators/Fuzz.js
@@ -2,8 +2,11 @@
   'use strict';
 
   b3e.nodes.Fuzz = b3e.node.create('Fuzz', b3e.MODULATOR, {
+    maxInConnections: 1,
     properties: [
-      ['curve', b3e.properties.Curve, {}],
+      ['curve', b3e.properties.Curve, {
+        lockYDomain: true,
+      }],
     ]
   });
 })();
